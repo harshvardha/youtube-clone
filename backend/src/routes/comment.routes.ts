c9e3b0ccccd552358fd4786eb94eda49670e95cf
harshvardhan singh <harshvardhansingh458@protonmail.com>
2022-12-14 17:04:43 +0530

    added all the routes

--- backend/src/routes/comment.routes.ts
+++ backend/src/routes/comment.routes.ts
@@ -1,11 +1,44 @@
 import { Router } from "express";
-import { addComment, deleteComment, getComments } from "../controllers/comment.controllers.js";
-import verifyToken from "../middlewares/verifyAccessToken.middleware.js";
+import { check } from "express-validator";
+import {
+    addComment,
+    deleteComment,
+    deleteReplyComment,
+    dislikeComment,
+    dislikeReplyComment,
+    getComments,
+    likeComment,
+    likeReplyComment,
+    replyToComment,
+    updateComment,
+    updateReplyComment
+} from "../controllers/comment.controllers";
+import verifyToken from "../middlewares/verifyAccessToken.middleware";
 
 const commentRouter = Router();
 
-commentRouter.post("/", verifyToken, addComment);
-commentRouter.delete("/:commentId", verifyToken, deleteComment);
-commentRouter.get("/:videoId", getComments);
+commentRouter.post(
+    "/add",
+    [
+        check("description").isLength({ min: 1 })
+    ],
+    verifyToken,
+    addComment);
+commentRouter.delete("/delete/:commentId", verifyToken, deleteComment);
+commentRouter.get("/getComments/:videoId", getComments);
+commentRouter.put("/like/:commentId", verifyToken, likeComment);
+commentRouter.put("/dislike/:commentId", verifyToken, dislikeComment);
+commentRouter.patch("/update/:commentId", verifyToken, updateComment);
+commentRouter.post(
+    "/reply/:commentId",
+    [
+        check("description").isLength({ min: 1 })
+    ],
+    verifyToken,
+    replyToComment);
+commentRouter.put("/reply/like/:commentId", verifyToken, likeReplyComment);
+commentRouter.put("/reply/dislike/:commentId", verifyToken, dislikeReplyComment);
+commentRouter.patch("/reply/update/:commentId", verifyToken, updateReplyComment);
+commentRouter.delete("/reply/delete/:commentId", verifyToken, deleteReplyComment);
 
 export default commentRouter;
