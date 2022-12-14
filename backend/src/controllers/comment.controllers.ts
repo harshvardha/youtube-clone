a0bdc4c9d2be5d2f5b9f962ccf40cc6cccb8dc6b
harshvardhan singh <harshvardhansingh458@protonmail.com>
2022-12-14 16:48:19 +0530

    added functionalities for reply comments

--- backend/src/controllers/comment.controllers.ts
+++ backend/src/controllers/comment.controllers.ts
@@ -14,7 +14,7 @@ const addComment = async (req: Request | any, res: Response, next: NextFunction)
         const userId = req.user.id;
         const videoId = req.params.videoId;
         const { description } = req.body;
-        const newComment = await Comment.create({
+        await Comment.create({
             userId,
             videoId,
             description
@@ -101,10 +101,141 @@ const dislikeComment = async (req: Request | any, res: Response, next: NextFunct
     }
 }
 
+const updateComment = async (req: Request | any, res: Response, next: NextFunction) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return next(new CustomError(StatusCodes.UNPROCESSABLE_ENTITY, "Please provide proper description."));
+        }
+        const userId = req.user.id;
+        const commentId = req.params.id;
+        const comment = await Comment.findById(commentId);
+        if (comment?.userId === userId) {
+            await comment?.updateOne({
+                $set: { description: req.body.description }
+            });
+            res.status(StatusCodes.OK).json({ message: "Comment Updated" });
+        }
+        else {
+            next(new CustomError(StatusCodes.UNAUTHORIZED, "You are only authorized to update your comments."));
+        }
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+}
+
+const replyToComment = async (req: Request | any, res: Response, next: NextFunction) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return next(new CustomError(StatusCodes.UNPROCESSABLE_ENTITY, "Please provide proper description."));
+        }
+        const userId = req.user.id;
+        const commentId = req.params.commentId;
+        const comment = await Comment.findById(commentId);
+        await comment?.updateOne({
+            replies: { $set: { userId: userId, description: req.body.description } }
+        })
+        res.status(StatusCodes.OK).json({ message: "Reply added." });
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+}
+
+const likeReplyComment = async (req: Request | any, res: Response, next: NextFunction) => {
+    try {
+        const userId = req.user.id;
+        const commentId = req.params.commentId;
+        const comment = await Comment.findById(commentId);
+        const replyComment = comment?.replies.find((reply: any) => reply._id.toString() === req.body.replyCommentId);
+        if (replyComment?.dislikes.includes(userId)) {
+            replyComment.dislikes = replyComment.dislikes.filter(dislike => dislike !== userId);
+        }
+        replyComment?.likes.push(userId);
+        res.status(StatusCodes.OK).json({ message: "Like added." });
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+}
+
+const dislikeReplyComment = async (req: Request | any, res: Response, next: NextFunction) => {
+    try {
+        const userId = req.user.id;
+        const commentId = req.params.commentId;
+        const comment = await Comment.findById(commentId);
+        const replyComment = comment?.replies.find((reply: any) => reply._id.toString() === req.body.replyCommentId);
+        if (replyComment?.likes.includes(userId)) {
+            replyComment.likes = replyComment.likes.filter(like => like !== userId);
+        }
+        replyComment?.dislikes.push(userId);
+        res.status(StatusCodes.OK).json({ message: "Dislike added." });
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+}
+
+const updateReplyComment = async (req: Request | any, res: Response, next: NextFunction) => {
+    try {
+        const userId = req.user.id;
+        const commentId = req.params.commentId;
+        const comment = await Comment.findById(commentId);
+        const replyComment = comment?.replies.find((reply: any) => reply._id.toString() === req.body.replyCommentId);
+        if (replyComment && replyComment.userId.toString() === userId) {
+            replyComment.description = req.body.description;
+            res.status(StatusCodes.OK).json({ message: "Reply updated." });
+        }
+        else if (!replyComment) {
+            return next(new CustomError(StatusCodes.NOT_FOUND, "Reply Comment Not Found."));
+        }
+        else if (replyComment.userId.toString() === userId) {
+            next(new CustomError(StatusCodes.UNAUTHORIZED, "You can only update your comment."));
+        }
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+}
+
+const deleteReplyComment = async (req: Request | any, res: Response, next: NextFunction) => {
+    try {
+        const userId = req.user.id;
+        const commentId = req.params.commentId;
+        const comment = await Comment.findById(commentId);
+        const replyComments = comment?.replies;
+        const filterdReplyComments = replyComments?.filter((reply: any) => {
+            if (reply._id.toString() === req.body.replyCommentId) {
+                if (reply.userId.toString() !== userId) {
+                    throw new CustomError(StatusCodes.UNAUTHORIZED, "You are only allowed to delete your comments.");
+                }
+            }
+            else {
+                return reply;
+            }
+        })
+        await comment?.updateOne({
+            $set: { replies: filterdReplyComments }
+        });
+        res.status(StatusCodes.OK).json({ message: "Comment Deleted." });
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+}
+
 export {
     addComment,
     deleteComment,
     getComments,
     likeComment,
-    dislikeComment
+    dislikeComment,
+    updateComment,
+    replyToComment,
+    likeReplyComment,
+    dislikeReplyComment,
+    updateReplyComment,
+    deleteReplyComment
 }
