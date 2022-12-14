5d1d382ff48c50d926949b4ccc8417516a27c761
harshvardhan singh <harshvardhansingh458@protonmail.com>
2022-12-14 17:06:06 +0530

    activated video and comment routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,8 +5,8 @@ import CustomError from "./types/CustomError";
 import { Request, Response, NextFunction } from "express";
 import userRouter from "./routes/user.routes";
 import authenticationRouter from "./routes/authentication.routes";
-// import commentRouter from "./routes/comment.routes.js";
-// import videoRouter from "./routes/video.routes.js";
+import commentRouter from "./routes/comment.routes.js";
+import videoRouter from "./routes/video.routes.js";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -21,8 +21,8 @@ app.use(express.json());
 
 app.use("/authentication", authenticationRouter);
 app.use("/users", userRouter);
-// app.use("/comments", commentRouter);
-// app.use("/videos", videoRouter);
+app.use("/comments", commentRouter);
+app.use("/videos", videoRouter);
 
 app.use((error: CustomError, req: Request, res: Response, next: NextFunction) => {
     const status = error.status || 500;
